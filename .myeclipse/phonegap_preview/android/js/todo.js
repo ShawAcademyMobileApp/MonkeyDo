84a99bebe9a4d7b8d3fcb0f815586b55d2c02b31
shaw-mobile <michael.dunne@shawacademy.com>
2016-07-20 19:08:01 +0100

    Lesson 7 Broken
    LocalStorage not Updating

---
+++ .myeclipse/phonegap_preview/android/js/todo.js
@@ -0,0 +1,37 @@
+function getArray()
+{
+	var toDoList = new Array;						//Create An Array
+	var taskStr = localStorage.getItem(toDoList);		//Get the Task from Input using LocalStorage
+	if(taskStr !== null)								//if the value is NOT empty --null--
+		{
+			toDoList = JSON.parse(taskStr);
+		}
+	return toDoList;
+}
+
+function addTask()
+{
+	var task = document.getElementById("taskName").value;			//create a variable to hold value of input
+	var array = getArray();							//create a variable to hold our Array
+	array.push(task);												//pushing the task into the Array
+	localStorage.setItem(array, JSON.stringify(task));		//Store the task localStorage saving to the Array
+	show();
+	
+	return false;
+}
+
+function show()
+{
+	var array = getArray();			//create the variable to hold Array
+	var htmlFormat = "<ul>";		//Add unordered List
+	
+	for (var i=0; i < array.length; i++)		//A For Loop to display the Array
+		{
+			htmlFormat += "<li>" + array[i] + "</li>";
+		}
+	htmlFormat += "</ul>";
+	document.getElementById(array).innerHTML = htmlFormat;			//Show the document
+}
+
+document.getElementById('add').addEventListener('click', addTask);
+show();
